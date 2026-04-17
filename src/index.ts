331687cb77a534a22d83a62b5bd8a4aaa4e3f938
mahdixmohammad <moham52@mcmaster.ca>
2026-04-17 17:33:33 -0400

    Add Prettier default formatting

--- src/index.ts
+++ src/index.ts
@@ -2,56 +2,59 @@ require("./index.scss");
 require("./particles.js");
 require("./Mahdi-Mohammad-Resume.pdf");
 
-("use strict");
-// DOM variables
 const nav: HTMLElement = document.querySelector("nav")!;
 const hamburger: HTMLElement = document.querySelector("#hamburger")!;
-const navlinks: NodeListOf<HTMLElement> = document.querySelectorAll("nav a, #contactbtn");
+const navlinks: NodeListOf<HTMLElement> =
+  document.querySelectorAll("nav a, #contactbtn");
 const reveals: NodeListOf<HTMLElement> = document.querySelectorAll(".popin");
-const progressBars: NodeListOf<HTMLElement> = document.querySelectorAll(".progress");
+const progressBars: NodeListOf<HTMLElement> =
+  document.querySelectorAll(".progress");
 
-// enables dropdown menu with nav links when hamburger is clicked (small screen sizes only)
+// Enables dropdown menu with nav links when hamburger is clicked (small screen sizes only).
 hamburger.onclick = (): void => {
-	nav.classList.toggle("active");
+  nav.classList.toggle("active");
 };
 
-// remove active nav when user clicks nav link
+// Remove active nav when user clicks nav link.
 navlinks.forEach((navlink: HTMLElement): void => {
-	navlink.onclick = (): void => {
-		nav.classList.remove("active");
-	};
+  navlink.onclick = (): void => {
+    nav.classList.remove("active");
+  };
 });
 
-// loads the width of all the progress bars in skills section automatically
+// Loads the width of all the progress bars in skills section automatically.
 progressBars.forEach((progressBar: HTMLElement): void => {
-	progressBar.querySelector("div")!.style.width = `${progressBar.textContent}`;
+  progressBar.querySelector("div")!.style.width = `${progressBar.textContent}`;
 });
 
-// as user scrolls down, sections pop in
+// As user scrolls down, sections pop in.
 function popInAnimation(): void {
-	reveals.forEach((element: HTMLElement): void => {
-		let elementVisible: number = element.clientHeight * 0.09;
-		if (elementVisible < 30) elementVisible = 30;
-		else if (elementVisible > 60) elementVisible = 60;
+  reveals.forEach((element: HTMLElement): void => {
+    let elementVisible: number = element.clientHeight * 0.09;
+    if (elementVisible < 30) elementVisible = 30;
+    else if (elementVisible > 60) elementVisible = 60;
 
-		if (element.classList.contains("project")) elementVisible = 20;
-		let elementTop: number = element.getBoundingClientRect().top;
-		let distanceFromView: number = Math.max(0, window.innerHeight - elementTop - elementVisible);
-		let opacity: number = (distanceFromView / elementVisible) * 0.4 - 1;
-		let scale: number = (distanceFromView / elementVisible) * 0.1 + 0.25;
+    if (element.classList.contains("project")) elementVisible = 20;
+    let elementTop: number = element.getBoundingClientRect().top;
+    let distanceFromView: number = Math.max(
+      0,
+      window.innerHeight - elementTop - elementVisible,
+    );
+    let opacity: number = (distanceFromView / elementVisible) * 0.4 - 1;
+    let scale: number = (distanceFromView / elementVisible) * 0.1 + 0.25;
 
-		if (scale >= 1) {
-			scale = 1;
-			element.classList.add("visible");
-		} else {
-			element.classList.remove("visible");
-		}
-		element.style.opacity = `${opacity}`;
-		element.style.transform = `scale(${scale})`;
-	});
+    if (scale >= 1) {
+      scale = 1;
+      element.classList.add("visible");
+    } else {
+      element.classList.remove("visible");
+    }
+    element.style.opacity = `${opacity}`;
+    element.style.transform = `scale(${scale})`;
+  });
 }
 
 window.addEventListener("scroll", popInAnimation);
 
-// checks the scroll position on page load
+// Checks the scroll position on page load.
 popInAnimation();
--- webpack.config.js
+++ webpack.config.js
@@ -3,43 +3,43 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-	mode: "production",
-	entry: "./src/index.ts",
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: "./src/index.html",
-		}),
-		new MiniCssExtractPlugin({
-			filename: "index.css",
-		}),
-	],
-	output: {
-		filename: "index.js",
-		path: path.resolve(__dirname, "dist"),
-		clean: true,
-	},
-	module: {
-		rules: [
-			{
-				test: /\.ts$/,
-				loader: "ts-loader",
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.html$/,
-				loader: "html-loader",
-			},
-			{
-				test: /\.scss$/,
-				use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-			},
-			{
-				test: /\.pdf$/,
-				type: "asset/resource",
-				generator: {
-					filename: "[name][ext]",
-				},
-			},
-		],
-	},
+  mode: "production",
+  entry: "./src/index.ts",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "index.css",
+    }),
+  ],
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.html$/,
+        loader: "html-loader",
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.pdf$/,
+        type: "asset/resource",
+        generator: {
+          filename: "[name][ext]",
+        },
+      },
+    ],
+  },
 };
